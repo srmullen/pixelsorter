81822581ff1a45caa7ca78827f08b2bdadb02465
Sean Mullen <srmullen13@gmail.com>
2020-02-12 12:55:19 -0700

    update paper

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,60 +4,60 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const DashboardPlugin = require("webpack-dashboard/plugin");
 
 module.exports = {
-    devtool: "source-map",
-    mode: 'development',
-    entry: {
-        "demo": "./src/pages/demo",
-        "pixelsorter": "./src/pages/pixelsorter"
-    },
-    output: {
-        path: path.join(__dirname, "dist"),
-        filename: "[name].js"
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            filename: "demo.html",
-            template: "./src/index.html",
-            chunks: ["demo"]
-        }),
-        new HtmlWebpackPlugin({
-            filename: "pixelsorter.html",
-            template: "./src/index.html",
-            chunks: ["pixelsorter"]
-        })
-    ],
-    resolve: {
-        alias: {
-            components: path.resolve(__dirname, "src/components"),
-            utils: path.resolve(__dirname, "src/utils"),
-            sort: path.resolve(__dirname, "src/sort"),
-            images: path.resolve(__dirname, "images"),
-            root: path.resolve(__dirname, "src"),
-            styles: path.resolve(__dirname, "src/styles")
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.js$/,
-                use: ["babel-loader"],
-                exclude: /node_modules/
-            }
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/,
-            use: [
-              'file-loader'
-            ]
-          }
-        ]
-    },
-    devServer: {
-        contentBase: __dirname,
-        index: "pixelsorter.html",
-        port: 3400
+  devtool: "source-map",
+  mode: 'development',
+  entry: {
+    "demo": "./src/pages/demo",
+    "pixelsorter": "./src/pages/pixelsorter"
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js"
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      filename: "demo.html",
+      template: "./src/index.html",
+      chunks: ["demo"]
+    }),
+    new HtmlWebpackPlugin({
+      filename: "pixelsorter.html",
+      template: "./src/index.html",
+      chunks: ["pixelsorter"]
+    })
+  ],
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, "src/components"),
+      utils: path.resolve(__dirname, "src/utils"),
+      sort: path.resolve(__dirname, "src/sort"),
+      images: path.resolve(__dirname, "images"),
+      root: path.resolve(__dirname, "src"),
+      styles: path.resolve(__dirname, "src/styles")
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.js$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: __dirname,
+    index: "pixelsorter.html",
+    port: 3400
+  }
 }
